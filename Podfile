04f022e1a13344be18f2718e4eda5b6fa6b83a1a
liugangyi <liugangyi@tianfutong.org>
2019-11-15 11:01:14 +0800

    modify

--- Podfile
+++ Podfile
@@ -1,5 +1,5 @@
 # Uncomment the next line to define a global platform for your project
-platform :ios, '9.0'
+platform :ios, '10.0'
 
 target 'PracticeRxSwift' do
   # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
@@ -8,4 +8,6 @@ target 'PracticeRxSwift' do
   # Pods for PracticeRxSwift
   pod 'RxSwift'
   pod 'RxCocoa'
+  pod 'Alamofire', '~> 5.0.0-rc.3'
+  pod 'HandyJSON', '~> 5.0.1'
 end
--- PracticeRxSwift/ViewController.swift
+++ PracticeRxSwift/ViewController.swift
@@ -7,11 +7,44 @@
 //
 
 import UIKit
+import Foundation
+import Alamofire
+import HandyJSON
 
 class ViewController: UIViewController {
 
+    struct ServerRresult<T:HandyJSON>: HandyJSON {
+        var code:Int = 0
+        var data: T?
+        var dataList: [T]?
+        var msg:String?
+        mutating func mapping(mapper: HelpingMapper) {
+            
+        }
+    }
+    
+    struct Product:HandyJSON {
+        
+        var arrangedCount = 0
+        var classTime:String = ""
+        var subjectTypeId:String = ""
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let baseUrl = "http://test03-wangxiao.coolmath.cn"
+        let path = "/mobile/course/list.json"
+        let allPath = baseUrl+path
+        let paragram = ["subjectTypeId":"94","pageSize":"1","pageNum":"1"]
+
+        
+        
+        AF.request(allPath, method: .post, parameters: paragram, encoding: URLEncoding.default).responseJSON { response in
+            print(response.value! as! NSDictionary)
+            let result = ServerRresult<Product>.deserialize(from: response.value as! NSDictionary)
+        }
+        
         
         
     }
